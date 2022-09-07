f70b75ecd3d4e2cd523c4f5a5d5e791f9e6a995e
LeeviKamarainen <63156337+LeeviKamarainen@users.noreply.github.com>
2022-09-07 15:18:31 +0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,35 @@
+import "./styles.css";
+
+if (document.readyState !== "loading") {
+  console.log("Document is ready!");
+  initializeCode();
+} else {
+  document.addEventListener("DOMContentLoaded", function () {
+    console.log("Document is ready after waiting!");
+    initializeCode();
+  });
+}
+
+function initializeCode() {
+  const changeTextButton = document.getElementById("my-button");
+  const addDataButton = document.getElementById("add-data");
+  changeTextButton.addEventListener("click", function () {
+    changeText();
+  });
+  addDataButton.addEventListener("click", function () {
+    addData();
+  });
+
+  function changeText() {
+    console.log("hello world");
+    document.getElementById("maintext").innerHTML = "My notebook";
+  }
+
+  function addData() {
+    //Inspiration for how to add child elements from here:https://stackoverflow.com/questions/20673959/how-to-add-new-li-to-ul-onclick-with-javascript
+    var li = document.createElement("li");
+    var textAdd = document.getElementById("text-data").value;
+    li.appendChild(document.createTextNode(textAdd));
+    document.getElementById("list1").appendChild(li);
+  }
+}
